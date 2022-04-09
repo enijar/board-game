2f618af47e0a3e74fd4c097fee6a4bf8a2a558f3
Enijar <TheEnijar@gmail.com>
2022-04-09 16:13:59 +0100

    feat: add tile layout to board

--- client/src/game/components/board/board.tsx
+++ client/src/game/components/board/board.tsx
@@ -2,8 +2,74 @@ import React from "react";
 import * as THREE from "three";
 import Tile from "@/game/components/board/tile";
 
-export default function Board() {
-  const [hoveringTile, setHoveringTile] = React.useState(false);
+type Props = {
+  width?: number;
+  height?: number;
+  originalWidth?: number;
+  originalHeight?: number;
+};
+
+const tiles = [
+  {
+    id: 1,
+    x: 61.5,
+    y: 310.5,
+    width: 209,
+    height: 160.5,
+  },
+  {
+    id: 2,
+    x: 222,
+    y: 39.5,
+    width: 257.5,
+    height: 358,
+  },
+  {
+    id: 3,
+    x: 26.5,
+    y: 147.5,
+    width: 244,
+    height: 163.5,
+  },
+  {
+    id: 4,
+    x: 117.5,
+    y: 31.5,
+    width: 153,
+    height: 162,
+  },
+  {
+    id: 5,
+    x: 39.5,
+    y: 54.5,
+    width: 104,
+    height: 107,
+  },
+];
+
+export default function Board({
+  width = 4,
+  height = 4,
+  originalWidth = 500,
+  originalHeight = 500,
+}: Props) {
+  const [hoveringTileId, setHoveringTileId] = React.useState(0);
+  const [selectedTileId, setSelectedTileId] = React.useState(0);
+
+  const scale = width / originalWidth;
+
+  const getColor = React.useCallback(
+    (tile): THREE.ColorRepresentation => {
+      if (selectedTileId === tile.id) {
+        return "#00ff00";
+      }
+      if (hoveringTileId === tile.id) {
+        return "#999999";
+      }
+      return "#000000";
+    },
+    [selectedTileId, hoveringTileId]
+  );
 
   return (
     <group
@@ -11,26 +77,50 @@ export default function Board() {
       position={[0, 0.2, 0]}
     >
       <mesh>
-        <planeBufferGeometry args={[6, 4]} />
+        <planeBufferGeometry args={[width, height]} />
         <meshBasicMaterial
           color="crimson"
           side={THREE.DoubleSide}
           depthWrite={false}
         />
       </mesh>
-      <Tile
-        image="/tile.svg"
-        color={hoveringTile ? "#999999" : "#000000"}
-        onPointerOver={() => {
-          setHoveringTile(true);
-        }}
-        onPointerOut={() => {
-          setHoveringTile(false);
-        }}
-        onPointerDown={() => {
-          console.log("onPointerDown");
-        }}
-      />
+      <group>
+        {tiles.map((tile) => {
+          const x = THREE.MathUtils.mapLinear(
+            tile.x,
+            0,
+            originalWidth,
+            originalWidth * -0.5,
+            originalWidth * 0.5
+          );
+          const y = THREE.MathUtils.mapLinear(
+            tile.y,
+            0,
+            originalHeight,
+            originalHeight * 0.5,
+            originalHeight * -0.5
+          );
+          return (
+            <Tile
+              key={tile.id}
+              image={`/tiles/${tile.id}.svg`}
+              color={getColor(tile)}
+              scale={scale}
+              x={x}
+              y={y}
+              onPointerOver={() => {
+                setHoveringTileId(tile.id);
+              }}
+              onPointerOut={() => {
+                setHoveringTileId(0);
+              }}
+              onPointerDown={() => {
+                setSelectedTileId(tile.id);
+              }}
+            />
+          );
+        })}
+      </group>
     </group>
   );
 }
--- client/src/game/components/board/tile.tsx
+++ client/src/game/components/board/tile.tsx
@@ -6,6 +6,8 @@ import { ThreeEvent, useLoader } from "@react-three/fiber";
 type Props = {
   image: string;
   scale?: number;
+  x?: number;
+  y?: number;
   color: THREE.ColorRepresentation;
   onPointerOver?: (event: ThreeEvent<PointerEvent>) => void;
   onPointerOut?: (event: ThreeEvent<PointerEvent>) => void;
@@ -14,46 +16,44 @@ type Props = {
 
 export default function Tile({
   image,
+  scale = 1,
+  x = 0,
+  y = 0,
   color,
   onPointerOver,
   onPointerOut,
   onPointerDown,
-  scale = 0.005,
 }: Props) {
   const svg = useLoader(SVGLoader, image);
 
   return (
     <group
-      scale={[scale, scale * -1, scale]}
+      scale={scale}
       onPointerOver={onPointerOver}
       onPointerOut={onPointerOut}
       onPointerDown={onPointerDown}
     >
-      {svg.paths.map((path, index) => {
-        const shapes = SVGLoader.createShapes(path);
-        return (
-          <React.Fragment key={index}>
-            {shapes.map((shape, index) => {
-              return (
-                <mesh key={index}>
-                  <shapeBufferGeometry
-                    ref={(shapeGeometry) => {
-                      if (shapeGeometry === null) return;
-                      shapeGeometry.center();
-                    }}
-                    args={[shape]}
-                  />
-                  <meshBasicMaterial
-                    color={color}
-                    side={THREE.DoubleSide}
-                    depthWrite={false}
-                  />
-                </mesh>
-              );
-            })}
-          </React.Fragment>
-        );
-      })}
+      <group position={[x, y, 0]}>
+        {svg.paths.map((path, index) => {
+          const shapes = SVGLoader.createShapes(path);
+          return (
+            <React.Fragment key={index}>
+              {shapes.map((shape, index) => {
+                return (
+                  <mesh key={index} scale={[1, -1, 1]}>
+                    <shapeBufferGeometry args={[shape]} />
+                    <meshBasicMaterial
+                      color={color}
+                      side={THREE.DoubleSide}
+                      depthWrite={false}
+                    />
+                  </mesh>
+                );
+              })}
+            </React.Fragment>
+          );
+        })}
+      </group>
     </group>
   );
 }
