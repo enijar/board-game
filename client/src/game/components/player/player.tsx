f1050bfbd13b088f6a3bf2f740b76b09928797e3
Enijar <TheEnijar@gmail.com>
2022-04-15 11:28:04 +0100

    chore: remove unused player code

--- client/src/game/components/player/player.tsx
+++ client/src/game/components/player/player.tsx
@@ -6,48 +6,6 @@ import { Action } from "@/game/types";
 import playerState from "@/game/state/player";
 import playerConfig from "@/game/config/player";
 import { Direction } from "@/types";
-import { TextureFilter } from "three/src/constants";
-
-function TextureAnimator(
-  texture: THREE.Texture,
-  tilesHoriz: number,
-  tilesVert: number,
-  numTiles: number,
-  tileDispDuration: number
-) {
-  // note: texture passed by reference, will be updated by the update function.
-
-  this.tilesHorizontal = tilesHoriz;
-  this.tilesVertical = tilesVert;
-  // how many images does this spritesheet contain?
-  //  usually equals tilesHoriz * tilesVert, but not necessarily,
-  //  if there at blank tiles at the bottom of the spritesheet.
-  this.numberOfTiles = numTiles;
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set(1 / this.tilesHorizontal, 1 / this.tilesVertical);
-
-  // how long should each image be displayed?
-  this.tileDisplayDuration = tileDispDuration;
-
-  // how long has the current image been displayed?
-  this.currentDisplayTime = 0;
-
-  // which image is currently being displayed?
-  this.currentTile = 0;
-
-  this.update = function (milliSec: number) {
-    this.currentDisplayTime += milliSec;
-    while (this.currentDisplayTime > this.tileDisplayDuration) {
-      this.currentDisplayTime -= this.tileDisplayDuration;
-      this.currentTile++;
-      if (this.currentTile == this.numberOfTiles) this.currentTile = 0;
-      const currentColumn = this.currentTile % this.tilesHorizontal;
-      texture.offset.x = currentColumn / this.tilesHorizontal;
-      const currentRow = Math.floor(this.currentTile / this.tilesHorizontal);
-      texture.offset.y = currentRow / this.tilesVertical;
-    }
-  };
-}
 
 export default function Player() {
   const actions = useActions();
